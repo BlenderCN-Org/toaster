fd17ae7e0f5f7eff6e41954371736eb5fc0db868
Jean-Francois Romang <jromang@protonmail.com>
2019-03-04 07:50:22 +0100

    Cleanup

--- __init__.py
+++ __init__.py
@@ -20,47 +20,44 @@ bl_info = {
 }
 
 
-#https://blender.stackexchange.com/questions/6975/is-it-possible-to-use-bpy-props-pointerproperty-to-store-a-pointer-to-an-object
+# https://blender.stackexchange.com/questions/6975/is-it-possible-to-use-bpy-props-pointerproperty-to-store-a-pointer-to-an-object
 class ToasterProperties(PropertyGroup):
-    SPP_DESC = ("Number of samples per pixel" )
-
-    spp : IntProperty(name="SPP", default=10, min=1, soft_max=100000, description=SPP_DESC)
-    #particle_object : StringProperty(name="Object", description="")
-    #particle_material : StringProperty(name="Material", description="")
+    SPP_DESC = "Number of samples per pixel"
+    spp: IntProperty(name="SPP", default=10, min=1, soft_max=100000, description=SPP_DESC)
 
 
 def draw_panel(self, context):
-    #https://docs.blender.org/api/blender2.8/bpy.types.UILayout.html#bpy.types.UILayout.prop
-    #https://docs.blender.org/api/blender2.8/info_quickstart.html
+    # https://docs.blender.org/api/blender2.8/bpy.types.UILayout.html#bpy.types.UILayout.prop
+    # https://docs.blender.org/api/blender2.8/info_quickstart.html
     layout = self.layout
     scene = context.scene
 
-    if context.engine=="toaster_renderer":
-        #view = context.space_data
-        layout.prop(scene.toaster, "spp" )
+    if context.engine == "toaster_renderer":
+        # view = context.space_data
+        layout.prop(scene.toaster, "spp")
 
 
 def register():
     print("Hello, REGISTER : "+__name__)
     # Register the RenderEngine
     bpy.utils.register_class(ToasterRenderEngine)
-    #bpy.utils.register_module(__name__)
+    # bpy.utils.register_module(__name__)
 
     # dictionaries can be assigned as long as they only use basic types.
-    #collection = bpy.data.collections.new("MyTestCollection")
-    #collection["MySettings"] = {"foo": 10, "bar": "spam", "baz": {}}
+    # collection = bpy.data.collections.new("MyTestCollection")
+    # collection["MySettings"] = {"foo": 10, "bar": "spam", "baz": {}}
 
     # RenderEngines also need to tell UI Panels that they are compatible
     # Otherwise most of the UI will be empty when the engine is selected.
     # In this example, we need to see the main render image button and
     # the material preview panel.
-    #from bl_ui import (
+    # from bl_ui import (
     #    properties_render,
-        #properties_material,
-    #)
-    #properties_render.RENDER_PT_render.COMPAT_ENGINES.add(ToasterRenderEngine.bl_idname)
+    #   properties_material,
+    # )
+    # properties_render.RENDER_PT_render.COMPAT_ENGINES.add(ToasterRenderEngine.bl_idname)
     bpy.types.RENDER_PT_context.append(draw_panel)
-    #properties_material.MATERIAL_PT_preview.COMPAT_ENGINES.add(CustomRenderEngine.bl_idname)
+    # properties_material.MATERIAL_PT_preview.COMPAT_ENGINES.add(CustomRenderEngine.bl_idname)
 
     bpy.utils.register_class(ToasterProperties)
     bpy.types.Scene.toaster = bpy.props.PointerProperty(type=ToasterProperties)
@@ -68,15 +65,15 @@ def register():
 
 def unregister():
     bpy.utils.unregister_class(ToasterRenderEngine)
-    #bpy.utils.unregister_module(__name__)
+    # bpy.utils.unregister_module(__name__)
 
-    #from bl_ui import (
+    # from bl_ui import (
     #    properties_render,
     #    properties_material,
-    #)
-    #properties_render.RENDER_PT_render.COMPAT_ENGINES.remove(CustomRenderEngine.bl_idname)
+    # )
+    # properties_render.RENDER_PT_render.COMPAT_ENGINES.remove(CustomRenderEngine.bl_idname)
     bpy.types.RENDER_PT_context.remove(draw_panel)
-    #properties_material.MATERIAL_PT_preview.COMPAT_ENGINES.remove(CustomRenderEngine.bl_idname)
+    # properties_material.MATERIAL_PT_preview.COMPAT_ENGINES.remove(CustomRenderEngine.bl_idname)
 
 
 if __name__ == "__main__":
